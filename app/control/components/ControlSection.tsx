e43b62fbfe4ea1fd81cf66991c4e39a6c85f7d6f
Szym <szymon.grabowiec2001@gmail.com>
2024-12-28 17:33:20 +0100

    Add note property to control section component

--- app/control/components/ControlSection.tsx
+++ app/control/components/ControlSection.tsx
@@ -5,21 +5,30 @@ import React from "react";
 interface ControlSectionI {
   title: string;
   children: React.ReactNode;
+  note?: string;
 }
 
-export default function ControlSection({ children, title }: ControlSectionI) {
+export default function ControlSection({
+  children,
+  title,
+  note = "",
+}: ControlSectionI) {
   const [isExpanded, setIsExpanded] = React.useState(false);
   return (
     <div className={`control-section ${isExpanded ? "--sectionExpanded" : ""}`}>
-      <div className="control-section__head">
-        <button
-          onClick={() => setIsExpanded((prev) => !prev)}
-          className="control-section__expand-toggle"
-        >
-          V
-        </button>
+      <div
+        className="control-section__head"
+        onClick={() => setIsExpanded((prev) => !prev)}
+      >
+        <img
+          src="/expand.svg"
+          className={`control-section__expand-toggle ${
+            isExpanded ? "--rotateToggle" : ""
+          }`}
+        />
         <p className="control-section__title">{title}</p>
       </div>
+      <p className="control-section__note">{note}</p>
       <div className={`control-section__content`}>{children}</div>
     </div>
   );
