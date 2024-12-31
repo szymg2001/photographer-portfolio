ba28920b8e178730d5fe99832f7595cc06acd95e
Szym <szymon.grabowiec2001@gmail.com>
2024-12-31 15:01:47 +0100

    Add footer

---
+++ app/components/Footer.tsx
@@ -0,0 +1,19 @@
+import Link from "next/link";
+import "../styles/footer.css";
+
+export default function Footer() {
+  return (
+    <div className="footer">
+      <h6 className="footer__title">Magdalena Pęcak</h6>
+      <div className="footer__nav-wrapper">
+        <Link href="/portfolio">Portfolio</Link>
+        <Link href="/">Contact</Link>
+      </div>
+      <div className="footer__media">
+        <img src={"/facebook.svg"} className="footer__media__button" />
+        <img src={"/insta.svg"} className="footer__media__button" />
+      </div>
+      <p className="footer__copy">© 2024</p>
+    </div>
+  );
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import { fetchFirebaseData } from "@/lib/api";
 import { AppContextProvider } from "@/lib/AppContext";
 import "./globals.css";
+import Footer from "./components/Footer";
 
 export default async function RootLayout({
   children,
@@ -12,7 +13,12 @@ export default async function RootLayout({
   return (
     <html lang="en">
       <body>
-        <AppContextProvider initialData={data}>{children}</AppContextProvider>
+        <AppContextProvider initialData={data}>
+          <>
+            {children}
+            <Footer />
+          </>
+        </AppContextProvider>
       </body>
     </html>
   );
