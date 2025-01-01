447d427caf0a9e86d8cc2c8181142bed422c507b
Szym <szymon.grabowiec2001@gmail.com>
2025-01-01 09:51:07 +0100

    Add font

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,6 +12,12 @@ export default async function RootLayout({
 
   return (
     <html lang="en">
+      <head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Arsenal:ital,wght@0,400;0,700;1,400;1,700&display=swap"
+          rel="stylesheet"
+        />
+      </head>
       <body>
         <AppContextProvider initialData={data}>
           <>
