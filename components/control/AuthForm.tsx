0c1dfd58983e6ad8552ad654f7b43fd6dbab4844
Szym <szymon.grabowiec2001@gmail.com>
2025-01-21 21:09:57 +0100

    Get value from autocomplete in authform

--- components/control/AuthForm.tsx
+++ components/control/AuthForm.tsx
@@ -6,12 +6,22 @@ export default function AuthForm() {
   const { handleLogin, authError } = useAppContext();
   const [formData, setFormData] = React.useState({ email: "", password: "" });
 
+  let emailRef = React.useRef<HTMLInputElement | null>(null);
+  let passRef = React.useRef<HTMLInputElement | null>(null);
+
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
-
-    let res = await handleLogin(formData.email, formData.password);
-    console.log(res);
+    await handleLogin(formData.email, formData.password);
   };
+
+  React.useLayoutEffect(() => {
+    if (emailRef.current && passRef.current) {
+      setFormData({
+        email: emailRef.current.value,
+        password: passRef.current.value,
+      });
+    }
+  }, []);
 
   return (
     <form className="auth-form" onSubmit={handleSubmit}>
@@ -25,6 +35,9 @@ export default function AuthForm() {
         type="email"
         className="auth-form__input"
         id="auth-form__email"
+        defaultValue={formData.email}
+        ref={emailRef}
+        autoComplete="email"
         onChange={(e) =>
           setFormData((prev) => ({ ...prev, email: e.target.value }))
         }
@@ -36,6 +49,9 @@ export default function AuthForm() {
         type="password"
         className="auth-form__input"
         id="auth-form__password"
+        defaultValue={formData.password}
+        ref={passRef}
+        autoComplete="current-password"
         onChange={(e) =>
           setFormData((prev) => ({ ...prev, password: e.target.value }))
         }
